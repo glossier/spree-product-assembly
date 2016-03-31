f89b5c25054428e6a0ed90099f9522b8b819df02
Bryan Mahoney <bryan@goDynamo.com>
2016-03-31 16:08:28 -0400

    Use custom order contents decorator from Glossier

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -2,23 +2,17 @@ module Spree
   OrderContents.class_eval do
 
     def add_to_line_item(variant, quantity, options = {})
-      line_item = grab_line_item_by_variant(variant, false, options)
+    line_item = grab_line_item_by_variant(variant, false, options)
 
-      if line_item && part_variants_match?(line_item, variant, quantity, options)
-        line_item.quantity += quantity.to_i
-        line_item.currency = currency unless currency.nil?
-      else
-        opts = { currency: order.currency }.merge ActionController::Parameters.new(options).
-                                            permit(PermittedAttributes.line_item_attributes)
-        line_item = order.line_items.new(quantity: quantity,
-                                          variant: variant,
-                                          options: opts)
-      end
-
-      line_item.target_shipment = options[:shipment] if options.has_key? :shipment
-      line_item.save!
-
-      line_item
+    if line_item && part_variants_match?(line_item, variant, quantity, options)
+      line_item.quantity += quantity.to_i
+      line_item.currency = currency unless currency.nil?
+    else
+      opts = { currency: order.currency }.merge ActionController::Parameters.new(options).
+                                          permit(Spree::PermittedAttributes.line_item_attributes)
+      line_item = order.line_items.new(quantity: quantity,
+                                        variant: variant,
+                                        options: opts)
     end
 
     def add_to_line_item_with_parts(variant, quantity, options = {})
@@ -26,8 +20,8 @@ module Spree
         tap do |line_item|
         populate_part_line_items(
           line_item,
-          variant.parts_variants,
-          options["selected_variants"]
+          variant.product.assemblies_parts,
+          options[:selected_variants]
         )
       end
     end
@@ -37,8 +31,8 @@ module Spree
     private
 
     def part_variants_match?(line_item, variant, quantity, options)
-      if line_item.parts.any? && options["selected_variants"]
-        selected_variant_ids = options["selected_variants"].values.map(&:to_i)
+      if line_item.parts.any? && options[:selected_variants]
+        selected_variant_ids = options[:selected_variants].map(&:to_i)
         matched = part_variant_ids(line_item) & selected_variant_ids
 
         matched == selected_variant_ids
@@ -51,6 +45,17 @@ module Spree
       line_item.part_line_items.map(&:variant_id)
     end
 
+    def populate_part_line_items(line_item, parts, selected_variants)
+      return if selected_variants.nil?
+      selected_variants.each do |variant|
+        part_line_item = line_item.part_line_items.find_or_initialize_by(
+          line_item: line_item,
+          variant_id: variant
+        )
+
+        part_line_item.update_attributes!(quantity: 1)
+      end
+    end
     def populate_part_line_items(line_item, parts, selected_variants)
       parts.each do |part|
         part_line_item = line_item.part_line_items.find_or_initialize_by(
